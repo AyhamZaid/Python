7044993d16bd2c6c264309964a8b9ff4e8d6a395
AyhamZaid <ayham.zaid.az@gmail.com>
2019-11-19 15:25:27 +0200

    EXDay1python

---
+++ EXday1.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+#Ayham Zaid
+Created on Tue Nov 19 12:49:58 2019
+
+@author: Ayham
+"""
+# =============================================================================
+# #Ex1
+# print("--------------------------------EX1")
+# print ("Hello")
+# print (" "," ","Hello")
+# print (" "," " ," ","Hello")
+# 
+# #Ex2
+# print("--------------------------------EX2")
+# print("OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy","OrnageAcademy" ,sep='\n')
+# 
+# #Ex3
+# print("--------------------------------EX3")
+# x=float(1)
+# y=float(2.8)
+# z=float("3")
+# w=float("4.2")
+# print(x,y,z,w,sep='10')
+# 
+# #Ex4
+# print("--------------------------------EX4")
+# num1 = 10
+# num2 = 24
+# result = num1 * num2
+# print(result)
+# 
+# #Ex5
+# print("--------------------------------EX5")
+# n1 = int(input("n1"))
+# n2 = int(input("n2"))
+# n3 = int(input("n3"))
+# n4 = int(input("n4"))
+# n5 = int(input("n5"))
+# Nresult=n1+n2+n3+n4+n5/5
+# print(Nresult)
+# #Ex6
+# print("--------------------------------EX6")
+# x=1
+# y=2.8
+# z=1j
+# o="Orange"
+# A=True
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(o))
+# print(type(A))
+# #Ex7
+# print("--------------------------------EX7")
+# x,y="Orange",1
+# X1,y1 = 100,-10
+# print(x)
+# print(y)
+# print(X1)
+# print(y1)
+# #Ex8 Correct the wrong
+# print("--------------------------------EX8 the result in comment")
+# #5x=10
+# #x_q$ ="Orange" #x_q ="Orange"
+# #print(X_q$) #print(X_q)
+# #A B = 14 AB = 14
+# #print(A B) print(AB)
+# #Print("10"*10)  Print(10*10)   
+# 
+# print("--------------------------------EX10")
+# x5= 10
+# print(x5)
+# X_q ="Orange"
+# print(X_q)
+# AB = 14
+# print(AB)
+# print(10*10)
+# 
+# #EX11
+# print("--------------------------------EX11")
+# #print("Hello,World!")
+# print("Cheers,Mate") # This is the program
+# """Testin
+# The Code
+# """
+# #EX12
+# print("--------------------------------EX12")
+# Name = str(input("Name"))
+# Age = int(input("Age"))
+# 
+# print("Enter Your Name" , Name)
+# 
+# print("Enter Your Age" , Age)
+# 
+# print ("your age after 100 year" , Age +100)
+# =============================================================================
+
+# =============================================================================
+# print("--------------------------------EX13")
+# base = int(input("base"))
+# hight = int(input("hight"))
+# print((base*hight)/2)
+# =============================================================================
+# =============================================================================
+# print("--------------------------------EX14")
+# x=11
+# y=3
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x%y)
+# print(abs(x*-1))
+# print(int(x))
+# print(float(x))
+# print(divmod(x,y))
+# print(pow(x,y))
+# print(x**y)
+# print(x>y)
+# print(x==y)
+# print(x !=y)
+# =============================================================================
+
+
+
+
+
